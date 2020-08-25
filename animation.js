1b4bc65690043d5873ac4e696b831f75aad9f30e
ayush-lab <ayush.millionaire8750@gmail.com>
2020-08-26 00:49:54 +0530

    added the shuttle parabola, net and heading

--- animation.js
+++ animation.js
@@ -1,47 +1,67 @@
+
 var object=document.querySelector(".shuttle img");
 var upward_movement=0;
 
 
-var timer= setInterval(move,2);
+var timer= setInterval(move,.0001);
 
 var pos=1;
 var pos_rotate=1;
-var flag=0;
+var flag=0,left_side=1;
+
 
 function move(){
-
-    if(pos == screen.width){
+  
+    if(parseInt(object.style.bottom)== 0){
       
         flag=1;
-        object.style.left = screen.width + 'px';
 
-    
     }
 
-    if(pos == 0){
+    if(parseInt(object.style.bottom)==0  && left_side ==1 ){
       
         flag=0;
-        object.style.left = 0 + 'px';
+        pos=1;
     }
 
+
+
     if(flag==0)
-        {
+        {   left_side=0;
             object.style.left =  + pos + 'px';
             pos+=1;
-            
-            if(parseInt(object.style.left) >=screen.width/6){
+            if( parseInt(object.style.left) >=screen.width/6  && parseInt(object.style.left) <=screen.width/4) {
                 // pos_rotate+=5;
                 upward_movement+=1/4;
-                object.style.left =  + pos + 'px';
+               // object.style.left =  + pos + 'px';
+                object.style.bottom =  + upward_movement + 'px';
+                //object.stsyle.bottom =  upward_movement+ 'px';
+            }
+             
+            else if( parseInt(object.style.left) >=screen.width/4  && parseInt(object.style.left) <=screen.width/2) {
+                // pos_rotate+=5;
+                upward_movement+=0.2;
+               // object.style.left =  + pos + 'px';
                 object.style.bottom =  + upward_movement + 'px';
                 //object.style.bottom =  upward_movement+ 'px';
-    
-            if(parseInt(object.style.left) >=screen.width/2){
+            }
+
+            else if(parseInt(object.style.left) >=screen.width/2 && parseInt(object.style.left)<screen.width/1.5){
+                    
+                        // object.style.left =  + pos + 'px';
+                        // upward_movement=0;
+                        object.style.bottom =  upward_movement+ 'px';
+
+            }
+
+            else if(parseInt(object.style.left) >=screen.width/1.5){
+                        
                 object.style.left =  + pos + 'px';
-                upward_movement-=1;
+                upward_movement-=0.7;
                 object.style.bottom =  upward_movement+ 'px';
-                                                 }
-        }
+
+    }
+
     
         else{
             upward_movement+=1;
@@ -52,32 +72,50 @@ function move(){
         }
         
         if(flag==1)
-        {
+        {  
+             left_side=1;
             
             pos-=1;
             object.style.left =  + pos + 'px';
 
-            if(parseInt(object.style.left) <=screen.width/1.19){
+            if(parseInt(object.style.left) <=screen.width/1.1 && parseInt(object.style.left) >screen.width/1.5){
                 // pos_rotate+=5;
-                upward_movement+=1/4;
-                object.style.left =  + pos + 'px';
-                object.style.bottom =  + upward_movement + 'px';
-                //object.style.bottom =  upward_movement+ 'px';
-    
-            if(parseInt(object.style.left) <=screen.width/2){
-                object.style.left =  + pos + 'px';
-                upward_movement-=1;
+               //alert("we");
+                upward_movement+=0.7;
+              //  object.style.left =  + pos + 'px';
+                //object.style.bottom =  + upward_movement + 'px';
                 object.style.bottom =  upward_movement+ 'px';
-                                                 }
-        }
+            }
+    
+            else if(parseInt(object.style.left) <=screen.width/1.5 && parseInt(object.style.left) >screen.width/1.8){
+
+                object.style.left =  + pos + 'px';
+                upward_movement-=0;
+                object.style.bottom =  upward_movement+ 'px';
+        
+            }
+        
+
+
+            else if(parseInt(object.style.left) <=screen.width/1.8){
+           // alert('asd');
+            object.style.left =  + pos + 'px';
+            upward_movement-=0.5;
+            object.style.bottom =  upward_movement+ 'px';
+                                        
+             }
+    
     
         else{
             upward_movement+=1;
-            object.style.bottom =  + upward_movement + 'px';}
+            object.style.bottom =  + upward_movement + 'px';
+           
+           }
         
         }
-            
-    
+
+
+
     object.style.transform = " rotate("+pos_rotate+"deg )";
     pos_rotate +=0.4;
     
@@ -85,6 +123,10 @@ function move(){
     console.log(pos);
     console.log(parseInt(object.style.left));
     console.log(screen.width/6);
+
+
+
+
 
 
 
